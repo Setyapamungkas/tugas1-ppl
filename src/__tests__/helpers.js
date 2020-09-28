f5372169a41e2d7e7829ef9de601d0b286a8d290
Wahyu Setyapamungkas <71322147+Setyapamungkas@users.noreply.github.com>
2020-09-28 11:37:45 +0700

    Add files via upload

---
+++ src/__tests__/helpers.js
@@ -0,0 +1,28 @@
+const {
+  incrementQty,
+  decrementQty,
+  recalculateSubtotal,
+  calculateDiscount,
+} = require("../helpers");
+
+test("qty 1 ditambah 1 samadengan 2", () => {
+  expect(incrementQty(1)).toBe(2);
+});
+test("qty 2 dikurangi 1 samadengan 1", () => {
+  expect(decrementQty(2)).toBe(1);
+});
+test("hitung diskon dengan kode", () => {
+  expect(calculateDiscount(250000, "Hello")).toBe(200000);
+});
+test("hitung diskon tanpa kode", () => {
+  expect(calculateDiscount(250000, "")).toBe(250000);
+});
+test("menghitung subtotal 25000 dengan barang 2 total menjadi 50000", () => {
+  expect(recalculateSubtotal(25000, 2)).toBe(50000);
+});
+test("perkalian harga yang isinya string", () => {
+  expect(recalculateSubtotal("25000", "2")).toBe(50000);
+});
+test("perkalian harga yang isinya string", () => {
+  expect(recalculateSubtotal("25000aaaaa", "2")).toBe("error");
+});
